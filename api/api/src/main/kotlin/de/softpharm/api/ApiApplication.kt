47f31a071ca5e943e126e8efa29f419bbd0b4a8c
Tobias Bück <buecktobias@gmail.com>
2022-08-04 19:27:42 +0200

    initial commit new Spring Boot Project

---
+++ api/api/src/main/kotlin/de/softpharm/api/ApiApplication.kt
@@ -0,0 +1,24 @@
+package de.softpharm.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiApplication>(*args)
+}
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Bonjour!"),
+		Message("3", "Privet!"),
+	)
+}
+
+data class Message(val id: String?, val text: String)
